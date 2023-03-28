80a145657d1331c4c15a617cb6009bc00a32d370
Nagornyh Roman <r.nagornyh@kuzstu.ru>
2023-03-28 15:14:39 +0700

    learing thearning

---
+++ main.py
@@ -0,0 +1,35 @@
+import itertools
+import time
+from threading import Thread, Event
+
+
+def spin(msg: str, done: Event):
+    for char in itertools.cycle(r'\|/'):
+        status = f'{char} {msg}'
+        print(status, end='', flush=True)
+        if done.wait(.1):
+            break
+        blanks = ' '*len(status)
+        print(f'\r{blanks}\r', end='')
+
+
+def slow() -> int:
+    time.sleep(3)
+    return 42
+
+
+def supervisor() -> int:
+    done = Event()
+    spinner = Thread(target=spin, args=('Ожидайте', done))
+    print(f'объект прокрутки:{spinner}')
+    spinner.start()
+    result = slow()
+    done.set()
+    spinner.join()
+    return result
+
+
+result = supervisor()
+print('')
+print('')
+print(f'Ответ : {result}')
